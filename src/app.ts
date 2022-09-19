9b5ce3ccc84d105c5f3d68154052d405ebbc902c
SoyaNyan <nyan@soya.moe>
2022-09-19 16:49:33 +0900

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ const app = express()
 // express setting
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: 'http://localhost:7766' }))
+app.use(cors({ origin: 'http://192.168.10.205:7766' }))
 
 // swagger router
 app.use(
